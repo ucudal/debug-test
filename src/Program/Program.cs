010d29278e783ce3677c7c46d0c485fab9e043e5
brunoalbin2311 <albinbruno0020@gmail.com>
2022-09-07 09:10:53 -0300

    Initial commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnitTestAndDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person("John Doe", "12345672");
+            Person jane = new Person("Jane Doe", "76543216");
+            john.IntroduceYourself();
+            jane.IntroduceYourself();
+        }
+    }
+}
